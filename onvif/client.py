8b1f41a7079285bd396e0893b6210ea041ae8e22
Martin <martin@technology-valley.com>
2024-12-28 15:39:37 +0200

    Fix issue when port is not specified

--- onvif/client.py
+++ onvif/client.py
@@ -240,13 +240,13 @@ class ONVIFCamera(object):
         capabilities = self.devicemgmt.GetCapabilities({'Category': 'All'})
         for name in capabilities:
             try:
-                retrived_address=capabilities[name].XAddr
-                right=retrived_address.split("//")[1]
-                retrived_url=right.split("/")[0]
-                ip_address=retrived_url.split(":")[0]
-                port_address = retrived_url.split(":")[1]
+                retrived_address = capabilities[name].XAddr
+                right = retrived_address.split("//")[1]
+                retrived_url = right.split("/")[0]
+                ip_address = retrived_url.split(":")[0]
+                port_address = retrived_url.split(":")[1] if ":" in retrived_url else "80"
+
                 if (self.host != ip_address or self.port != port_address):
-                    remaining=right.split("/")[1]
                     new_address="http://"+self.host+":"+str(self.port)+"/"+right.split("/")[1]+"/"+right.split("/")[2]
                     capabilities[name].XAddr=new_address
             except:
